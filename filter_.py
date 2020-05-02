f6bd2de163b8d9aae9e28ccfb442f11e2169800f
Pacome <Pacome@pacome.com>
2020-05-02 21:52:36 +0200

    add pass and stop band filters

--- filter_.py
+++ filter_.py
@@ -116,10 +116,11 @@ class BandPass(modules.ModuleBuilder):
             self.filter_freq = frequency
             self.filter_res = Q
             w = 2*np.pi * frequency / self.sr
-            norm = (1 + 2*Q/w + Q*w/2)
-            xlist = [1/norm, 0, -1/norm]
-            ylist = [(Q*w - 4*Q/w) / norm,
-                     (4*Q/w + Q*w/4 - 1) / norm]
+            alpha = (1 + 2*Q/w + Q*w/2)
+            beta = (Q*w - 4*Q/w)
+            gamma = (2*Q/w + Q*w/2 - 1)
+            xlist = [1/alpha, 0, -1/alpha]
+            ylist = [-beta/alpha, -gamma/alpha]
             if not self.iir_flag:
                 self.iir = Iir(ylist=ylist, xlist=xlist)
                 self.iir_flag = True
@@ -131,5 +132,46 @@ class BandPass(modules.ModuleBuilder):
             [Input, frequency, Q], [0, 504, 0.1]
         )
         self.make_filter(frequency, Q)
+        Output = self.iir(Input)
+        return {'Output':Output}
+
+##################################################
+
+class BandStop(modules.ModuleBuilder):
+    '''simple 1 pole high pass filter'''
+    def __init__(self, name='bandpass1'):
+        super().__init__(
+            name=name,
+            ins=['Input'], params=['frequency', 'wc'],
+            outs=['Output'],
+            function=self.filter_)
+        self.filter_freq = 504
+        self.filter_res = 0.1
+        self.iir_flag = False
+
+    def make_filter(self, frequency, wc):
+        if (frequency == self.filter_freq
+            and wc == self.filter_res):
+            return
+        else:
+            self.filter_freq = frequency
+            self.filter_res = wc
+            w0 = 2*np.pi * frequency / self.sr
+            xlist = np.array([w0**2 + 4, 2*w0**2 - 8, w0**2 + 4])
+            ylist = np.array([2*w0**2 - 8, w0**2 - 2*wc + 4])
+            norm = (w0**2 + 2*wc + 4)
+            xlist = xlist / norm
+            ylist = -ylist / norm
+            if not self.iir_flag:
+                self.iir = Iir(ylist=ylist, xlist=xlist)
+                self.iir_flag = True
+            else:
+                self.iir.update_xylist(ylist=ylist, xlist=xlist)
+
+    def filter_(self, Input=0, frequency=504, wc=0.1):
+        Input, frequency, wc = self._get_default_value(
+            [Input, frequency, wc], [0, 504, 0.1]
+        )
+        self.make_filter(frequency, wc)
         Output = self.iir(Input)
         return {'Output':Output}
